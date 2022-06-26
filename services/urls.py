17afe2c9d65a1f6d47892d850fef9ff0bcabf554
prince-cloud <acheampongprince227@gmail.com>
2022-06-26 19:32:59 +0000

    created api's for scan text and trnslate text

--- services/urls.py
+++ services/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
 
-    #path('scanned-text/', views.scan_text, name="scan_text"),
     path('scantext/', views.scan_text, name="scan_text"),
+    path('api/scantext/', views.scan_text_api, name="scan_text_api"),
     path('translate/', views.translateText, name="translateText"),
+    path('api/translate/', views.translateText_api, name="translateText_api"),
 ]
